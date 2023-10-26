a91bc5091fdbb30e91de82cadad98cf95a9e1729
somashekarprakruthi <114468337+somashekarprakruthi@users.noreply.github.com>
2023-10-25 20:38:06 -0400

    A7 (#8)
    * initial commit
    
    * added gitignore content
    
    * gitignore and README changes
    
    * security groups creation
    
    * ami value changes
    
    * added port
    
    * pulumi-aws dependency added
    
    * assignmnet 6 changes

--- README.md
+++ README.md
@@ -12,55 +12,88 @@ For first-time setup:
 
 ### Initialize the Pulumi project
 
-$ pulumi new aws-python
+```bash
+pulumi new aws-python
+```
 
 ### Create environments for dev and demo
-$ pulumi stack init dev
-$ pulumi stack init demo
+```bash
+pulumi stack init dev
+pulumi stack init demo
+```
 
 ### Configure the dev environment
-$ pulumi stack select dev
-$ pulumi config set aws:profile dev
+```bash
+pulumi stack select dev
+pulumi config set aws:profile dev
+```
 
 ### Configure the demo environment
-$ pulumi stack select demo
-$ pulumi config set aws:profile demo
+```bash
+pulumi stack select demo
+pulumi config set aws:profile demo
+```
 
 ### Define the CIDR block
-$ pulumi config set cidrBlock "10.0.0.0/16"
-$ pulumi config set customAmiId "ami-020b251b4f78f405a"
+```bash
+pulumi config set cidrBlock "10.0.0.0/16"
+pulumi config set customAmiId "ami-020b251b4f78f405a"
+```
 
 ### Deploy to the dev environment
-$ pulumi stack select dev
-$ AWS_PROFILE=dev pulumi up
+
+```bash
+pulumi stack select dev
+AWS_PROFILE=dev pulumi up
+```
 
 ### Deploy to the demo environment
-$ pulumi stack select demo
-$ AWS_PROFILE=demo pulumi up
-
+```bash
+pulumi stack select demo
+AWS_PROFILE=demo pulumi up
+```
 
 ### Check and Set the AWS region configured
-$ pulumi config get aws:region
-$ pulumi config set aws:region us-west-1
+```bash
+pulumi config get aws:region
+pulumi config set aws:region us-west-1
+```
 
 ### Destroy resources in the dev environment
-$ pulumi stack select dev
-$ pulumi destroy
+```bash
+pulumi stack select dev
+pulumi destroy
+```
 
 ### Destroy resources in the demo environment
-$ pulumi stack select demo
-$ pulumi destroy
+```bash
+pulumi stack select demo
+pulumi destroy
+```
 
 pulumi stack -> will give the stack selected
 
 
 If you change the region destroy the stack using the below command
-$ pulumi stack rm --force demo  
+```bash
+pulumi stack rm --force demo  
+```
+
+This will delete or create the resource which had problem in getting created or deleting the resource
+
+```bash
+pulumi refresh 
+```
 
 
+pulumi up
 
-pulumi refresh will delete or create the resourse which had problem in getting created or de
-
-
-pulumi up 
 pulumi down
+
+Set the Secret in Pulumi Configuration: You can use the Pulumi CLI to set the secret:
+
+```bash
+pulumi config set db_name "csye6225"
+pulumi config set db_username "csye6225"
+pulumi config set --secret dbPassword YOUR_PASSWORD_HERE
+```
--- __main__.py
+++ __main__.py
@@ -1,12 +1,16 @@
 import pulumi
 from pulumi_aws import ec2
 from pulumi_aws import get_availability_zones
+from pulumi_aws import rds
 
 config = pulumi.Config()
+db_name = config.require("db_name")
+db_username = config.require("username")
 
 vpc = ec2.Vpc('vpc',
     cidr_block= config.require("cidrBlock"),
     tags={'Name': 'my-vpc'})
+
 pulumi.export('vpc_id', vpc.id)
 
 # Retrieve the configurations
@@ -18,10 +22,6 @@ base_first_octet, base_second_octet = base_cidr_block.split('.')[0:2]
 # Get the availability zones for the region
 availability_zones_result = get_availability_zones()
 availability_zones = pulumi.Output.from_input(availability_zones_result.names).apply(lambda az: az[0:3])
-print(availability_zones_result)
-
-pulumi.export("VPC ID", vpc.id)
-
 
 # Create subnets
 public_subnets = availability_zones.apply(
@@ -47,9 +47,10 @@ private_subnets = availability_zones.apply(
     ]
 )
 
+pulumi.export("Private Subnets", private_subnets)
 # Log Subnets
-public_subnets.apply(lambda subnets: [print(f"Public Subnet Created: {subnet.id}") for subnet in subnets])
-private_subnets.apply(lambda subnets: [print(f"Private Subnet Created: {subnet.id}") for subnet in subnets])
+# public_subnets.apply(lambda subnets: [print(f"Public Subnet Created: {subnet.id}") for subnet in subnets if subnet is not None])
+# private_subnets.apply(lambda subnets: [print(f"Private Subnet Created: {subnet.id}") for subnet in subnets if subnet is not None])
 
 # Create an Internet Gateway and attach the Internet Gateway to the VPC.
 internet_gateway = ec2.InternetGateway("my-internet-gateway",
@@ -62,6 +63,8 @@ pulumi.export("Internet Gateway ID", internet_gateway.id)
 public_route_table = ec2.RouteTable("public-route-table",
     vpc_id=vpc.id
 )
+
+pulumi.export("Public route table", public_route_table.id)
 
 # Attach all public subnets to the public route table
 public_subnets.apply(
@@ -89,7 +92,7 @@ private_subnets.apply(
 )
 
 # Create a public route in the public route table
-route = ec2.Route("public-route",
+internet_gateway_route = ec2.Route("internet-gateway-route",
     route_table_id=public_route_table.id,
     destination_cidr_block="0.0.0.0/0",
     gateway_id=internet_gateway.id
@@ -127,37 +130,143 @@ app_security_group = ec2.SecurityGroup('app-security-group',
         ),
         # Add additional ingress rules for other ports as necessary
     ],
+    # Add an egress rule to allow outbound traffic to the RDS instance
     egress=[
         ec2.SecurityGroupEgressArgs(
-            protocol='-1',  # allow all protocols
-            from_port=0,
-            to_port=0,
+            protocol="tcp",  # allow all protocols
+            from_port=3306,
+            to_port=3306,
             cidr_blocks=['0.0.0.0/0']
         )
     ]
 )
 
-# 2. Create the EC2 instance
+
+# Create DB Security Group
+db_security_group = ec2.SecurityGroup('db-security-group',
+    vpc_id=vpc.id,
+    description='Security Group for RDS instances',
+    ingress=[
+        ec2.SecurityGroupIngressArgs(
+            protocol='tcp',
+            from_port=3306,  # For MySQL/MariaDB
+            to_port=3306,
+            security_groups=[app_security_group.id]  # Reference to the application security group
+        )
+    ]
+)
+
+# Create RDS Parameter Group for MySQL
+rds_parameter_group = rds.ParameterGroup("db-parameter-group",
+    family="mariadb10.6",
+    description="Database parameter group for Mariadb",
+    parameters=[
+        rds.ParameterGroupParameterArgs(
+            name="character_set_server",
+            value="utf8mb4"  # Set the character set to utf8mb4 which supports a wide range of characters including emojis
+        ),
+        rds.ParameterGroupParameterArgs(
+            name="character_set_client",
+            value="utf8mb4"
+        ),
+        rds.ParameterGroupParameterArgs(
+            name="collation_server",
+            value="utf8mb4_unicode_ci"  # Set the collation for utf8mb4
+        ),
+        rds.ParameterGroupParameterArgs(
+            name="max_connections",
+            value="100"  # Limit the maximum number of connections
+        ),
+        rds.ParameterGroupParameterArgs(
+            name="slow_query_log",
+            value="1"  # Enable the slow query log
+        ),
+        rds.ParameterGroupParameterArgs(
+            name="long_query_time",
+            value="2"  # Log queries that take more than 2 seconds
+        ),
+        # Add more parameters as necessary
+    ]
+)
+
+# Create a DB subnet group
+db_subnet_group = rds.SubnetGroup('db-subnet-group',
+                                  subnet_ids=private_subnets,
+                                  description='My DB subnet group',
+                                  tags={"Name": "db-subnet-group"})
+
+# Export the DB subnet group name
+pulumi.export('db_subnet_group_name', db_subnet_group.name)
+
+# Create RDS Instance
+rds_instance = rds.Instance("db-instance",
+    engine="mariadb",  # Choose your DB engine: 'mysql', 'mariadb', 'postgres', etc.
+    instance_class="db.t2.micro",
+    allocated_storage=20,
+    db_name=db_name,
+    username=db_username,
+    password=config.require_secret("dbPassword"),  #  keeping secrets like passwords in Pulumi config
+    parameter_group_name=rds_parameter_group.name,
+    skip_final_snapshot=True,
+    vpc_security_group_ids=[db_security_group.id],
+    db_subnet_group_name=db_subnet_group.name,  # Choose the appropriate subnet group
+    multi_az=False,
+    publicly_accessible=False
+)
+
+# Export the name of DB Instance
+pulumi.export('db_endpoint', rds_instance.id)
+
+
+# Create the EC2 instance
 ami_id = config.require("customAmiId")  # Ensure you set this value in your Pulumi configuration
 
+
+#User data generation
+user_data=pulumi.Output.all(rds_instance.endpoint, db_username, config.require_secret("dbPassword")).apply(
+        lambda args: f"""#!/bin/bash
+        sudo mkdir -p /opt/webappgroup/env/test
+        sudo echo 'Script executed successfully' > /opt/webappgroup/env/user-data-success.log
+        mkdir -p /opt/webappgroup/env
+        echo 'dbUrl=jdbc:mysql://{args[0]}/CloudDB?createDatabaseIfNotExist=true' > /opt/webappgroup/env/.env
+        echo 'dbUserName={args[1]}' >> /opt/webappgroup/env/.env
+        echo 'dbPass={args[2]}' >> /opt/webappgroup/env/.env
+        echo 'Script executed successfully' > /home/admin/user-data-success.log
+        cat /home/admin/env/.env | tee -a /home/admin/env-success.log
+        export DB_HOST={args[0]}
+        export DB_PORT=3306
+        export DB_NAME={args[1]}  # Replace with your actual database name
+        export DB_USERNAME={args[1]}
+        export DB_PASSWORD={args[2]}
+        """
+    )
+
+# EC2 Instance
 ec2_instance = ec2.Instance('my-ec2-instance',
-    instance_type='t2.micro',  # Choose the instance type as per your needs
+    instance_type='t2.micro',
     ami=ami_id,
     vpc_security_group_ids=[app_security_group.id],
-    subnet_id=public_subnets[0],  # Using the first public subnet; modify as needed
-    key_name='webapp',  # Replace with your key name
+    subnet_id=public_subnets[0].id,
+    key_name='webapp',
     ebs_block_devices=[
         ec2.InstanceEbsBlockDeviceArgs(
-            device_name='/dev/xvda',  # This can vary based on the AMI used
+            device_name='/dev/xvda',
             volume_type='gp2',
             volume_size=25,
-            delete_on_termination=True  # To ensure volume gets deleted on instance termination
+            delete_on_termination=True
         )
     ],
-    disable_api_termination=False,  # This ensures the instance isn't protected from accidental termination
+    instance_initiated_shutdown_behavior="stop",
+    root_block_device= ec2.InstanceRootBlockDeviceArgs(
+        volume_size=25,
+        volume_type='gp2',
+    ),
+    disable_api_termination=False,
     tags={
         'Name': 'my-ec2-instance'
-    }
+    },
+    user_data=user_data,
+    opts=pulumi.ResourceOptions(depends_on=[rds_instance])
 )
 
 pulumi.export('ec2_instance_id', ec2_instance.id)
